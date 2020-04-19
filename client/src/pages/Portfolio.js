625e9ce54af6089cef4257f78a712d45522d95c3
AndrewL <lawna12@gmail.com>
2020-04-18 22:40:55 -0400

    portfolio projects updated

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -2,12 +2,17 @@ import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import HeatWave from "../images/HeatWaveKY.png"
+import MyGarage from "../images/Garage-Rend_03_A.jpg"
+
 import PassGen from "../images/passGen.jpg"
 import BurgerApp from "../images/burgers.png"
 import DayPlanner from "../images/dayPlanner2pm.jpg"
-import NoteTaker from "../images/NoteTaker.jpg"
 import GitHub from "../images/github.jfif"
 import LinkedIn from "../images/linkedinImg.jfif"
+import GoudaBank from "../images/goudaBank.jpg"
+import EmpDirectory from "../images/employee-directory.jpg"
+
 
 
 
@@ -19,6 +24,29 @@ export default function Portfolio() {
                     <Col size="md-12">
                         <div className="main-section">
                             <h1>Portfolio</h1>
+                            <Row>
+                                <div class="col-md-4">
+                                    <div onclick="location.href='https://github.com/Lawna12/Heat-Wave-Crime-Data'" class="work" id="workImg">
+                                    <img src={HeatWave} className="auth-image" alt="HeatWave" />
+                                    <h3>P1. Heat Wave</h3>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">    
+                                    <div onclick="location.href='https://serene-scrubland-32480.herokuapp.com/members.html'" class="work" id="workImg">
+                                    <img src={GoudaBank} className="auth-image" alt="GoudaBank" />
+                                    <h3>P2. GoudaBank</h3>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div onclick="location.href='https://github.com/smaji08/myGarage';" class="work" id="workImg">
+                                    <img src={MyGarage} className="auth-image" alt="DayPlanner" />
+                                    <h3>P3. My Garage</h3>
+                                    </div>
+                                </div>
+                            </Row>
+
                             <Row>
                                 <div class="col-md-4">
                                     <div onclick="location.href='https://lawna12.github.io/passwordGenerator/'" class="work" id="workImg">
@@ -41,19 +69,19 @@ export default function Portfolio() {
                                     </div>
                                 </div>
                             </Row>
-
+                            
                             <Row>
-                                <div class="col-md-4">
-                                    <div onclick="location.href='https://github.com/Lawna12/noteTaker/tree/master/Develop';" class="work" id="workImg">
-                                    <img src={NoteTaker} className="auth-image" alt="NoteTaker" />
-                                    <h3>Note Taker</h3>
-                                    </div>
-                                </div>
-
                                 <div class="col-md-4">    
                                     <div onclick="location.href='https://github.com/Lawna12'" class="work" id="workImg">
                                     <img src={GitHub} className="auth-image" alt="GitHub" />
                                     <h3>GitHub</h3>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">    
+                                    <div onclick="location.href='https://serene-scrubland-32480.herokuapp.com/members.html'" class="work" id="workImg">
+                                    <img src={EmpDirectory} className="auth-image" alt="EmpDirectory" />
+                                    <h3>Employee Dir</h3>
                                     </div>
                                 </div>
                                 
