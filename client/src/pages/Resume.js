f38e11ad52990d2287bc830c765861e553d3f3ac
AndrewL <lawna12@gmail.com>
2020-04-18 22:00:36 -0400

    Resume page sorted

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -2,6 +2,8 @@ import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import ResumePDF from "../images/resumeJan2020.pdf"
+
 
 
 export default function Resume() {
@@ -11,7 +13,7 @@ export default function Resume() {
             <Row>
                 <Col size="md-12">
                     <div id="resume" className="main-section">
-                        
+                    <embed src={ResumePDF} type="application/pdf"   height="700px" width="600" />
                     </div>
                 </Col>
             </Row>
